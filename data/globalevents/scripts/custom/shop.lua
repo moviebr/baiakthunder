d4cba3e4a6c322569b9a6a0d785b74a1d0e4c3a7
Movie <contatobrenoalves@gmail.com>
2020-06-25 19:55:35 -0300

    Shop Update

--- data/globalevents/scripts/custom/shop.lua
+++ data/globalevents/scripts/custom/shop.lua
@@ -1,7 +1,4 @@
--- ### CONFIG ###
--- message to player "type", if delivery of item debugs client, it can be because of undefinied type (type that does not exist in your server LUA)
 SHOP_MSG_TYPE = MESSAGE_EVENT_ADVANCE
--- ### END OF CONFIG ###
 
 function onThink(interval)
     local resultId = db.storeQuery("SELECT * FROM z_ots_comunication")
@@ -13,90 +10,37 @@ function onThink(interval)
             if player then
                 local itemId = result.getDataInt(resultId, "param1")
                 local itemCount = result.getDataInt(resultId, "param2")
-                local containerId = result.getDataInt(resultId, "param3")
+                local outfitParam = (result.getDataString(resultId, "param3")):lower()
                 local containerItemsInsideCount = result.getDataInt(resultId, "param4")
                 local shopOfferType = result.getDataString(resultId, "param5")
                 local shopOfferName = result.getDataString(resultId, "param6")
 
 -- DELIVER ITEM
                 if shopOfferType == 'item' then
-                    local newItemUID = player:addItem(itemId, itemCount)
-                    --  item does not exist, wrong id OR count
-                    if not newItemUID then
-                        player:sendTextMessage(SHOP_MSG_TYPE, 'Website Shop bugged. Contact with administrator! Error is visible in server console.')
-                        print('ERROR! Website Shop (' .. player:getName() .. ') - cannot create item - invalid item ID OR count - ITEM ID: ' .. itemId .. ', ITEM COUNT: ' .. itemCount)
-                        return true
-                    end
-                    -- change item UniqueID to object of class Item
-                    local newItem = Item(newItemUID)
-
-                    -- add container with items to Store Inbox
-                    receivedItemStatus = player:addItem(newItem)
-
-                    if receivedItemStatus then
-                        player:sendTextMessage(SHOP_MSG_TYPE, 'You received ' .. shopOfferName .. ' from Website Shop. You can find your item in STORE INBOX (under EQ).')
+                    local item = Game.createItem(tonumber(itemId), tonumber(itemCount))
+                    local itemReceber = player:getInbox():addItemEx(item, INDEX_WHEREEVER, FLAG_NOLIMIT)
+                    if itemReceber == RETURNVALUE_NOERROR then
+                        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Você recebeu o item ".. ItemType(itemId):getName() .." no seu mailbox.")
+                        player:getPosition():sendMagicEffect(31)
                         db.asyncQuery("DELETE FROM `z_ots_comunication` WHERE `id` = " .. transactionId)
                         db.asyncQuery("UPDATE `z_shop_history_item` SET `trans_state`= 'realized', `trans_real`=" .. os.time() .. " WHERE `id` = " .. transactionId)
                     else
-                        player:sendTextMessage(SHOP_MSG_TYPE, 'Website Shop bugged. Contact with administrator! Error is visible in server console.')
-                        print('ERROR! Website Shop (' .. player:getName() .. ') - cannot add item to STORE INBOX - unknown reason, is it\'s size limited and it is full? - ITEM ID: ' .. itemId .. ', ITEM COUNT: ' .. itemCount)
-                    end
-
--- DELIVER CONTAINER
-                elseif shopOfferType == 'container' then
-                    -- create empty container
-                    local newContainerUID = player:addItem(containerId, 1)
-                    -- container item does not exist OR item is not Container
-                    if not newContainerUID or not Container(newContainerUID) then
-                        player:sendTextMessage(SHOP_MSG_TYPE, 'Website Shop bugged. Contact with administrator! Error is visible in server console.')
-                        print('ERROR! Website Shop (' .. player:getName() .. ') - cannot create container - invalid container ID - CONTAINER ID:' .. containerId)
-                        return true
-                    end
-                    -- change container UniqueID to object of class Container
-                    local newContainer = Container(newContainerUID)
-
-                    -- add items to container
-                    for i = 1, containerItemsInsideCount do
-                        -- create new item
-                        local newItemUID = player:addItem(itemId, itemCount)
-                        --  item does not exist, wrong id OR count
-                        if not newItemUID then
-                            player:sendTextMessage(SHOP_MSG_TYPE, 'Website Shop bugged. Contact with administrator! Error is visible in server console.')
-                            print('ERROR! Website Shop (' .. player:getName() .. ') - cannot create item - invalid item ID OR count - ITEM ID: ' .. itemId .. ', ITEM COUNT: ' .. itemCount)
-                            return true
-                        end
-                        -- change item UniqueID to object of class Item
-                        local newItem = Item(newItemUID)
-
-                        -- add item to container
-                        local addItemToContainerResult = newContainer:addItemEx(newItem)
-                        -- report error if it's not possible to add item to container
-                        if addItemToContainerResult then
-                            player:sendTextMessage(SHOP_MSG_TYPE, 'Website Shop bugged. Contact with administrator! Error is visible in server console.')
-                            print('ERROR! Website Shop (' .. player:getName() .. ') - cannot add item to container - item is not pickable OR variable "RETURNVALUE_NOERROR" is not definied in LUA - ITEM ID: ' .. itemId .. ', ITEM COUNT: ' .. itemCount)
-                            return true
-                        end
-                    end
-
-                    -- add container with items to Store Inbox
-                    receivedItemStatus = player:addItem(newContainer)
-
-                    if receivedItemStatus then
-                        player:sendTextMessage(SHOP_MSG_TYPE, 'You received ' .. shopOfferName .. ' from Website Shop. You can find your item in STORE INBOX (under EQ).')
-                        db.asyncQuery("DELETE FROM `z_ots_comunication` WHERE `id` = " .. transactionId)
-                        db.asyncQuery("UPDATE `z_shop_history_item` SET `trans_state`= 'realized', `trans_real`=" .. os.time() .. " WHERE `id` = " .. transactionId)
-                    else
-                        player:sendTextMessage(SHOP_MSG_TYPE, 'Website Shop bugged. Contact with administrator! Error is visible in server console.')
-                        print('ERROR! Website Shop (' .. player:getName() .. ') - cannot add container with items to STORE INBOX - unknown reason, is it\'s size limited and it is full? - ITEM ID: ' .. itemId .. ', ITEM COUNT: ' .. itemCount .. ', CONTAINER ID:' .. containerId .. ', ITEMS IN CONTAINER COUNT:' .. containerItemsInsideCount)
+                        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Houve algum erro ao tentar entregar um item do shopping. Entre em contato com os administradores")
+                        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+                        print("[SHOP] Erro ao tentar entregar o ".. ItemType(itemId):getName() .." para o ".. player:getName() .."." )
                     end
 
 -- DELIVER YOUR CUSTOM THINGS
                 elseif shopOfferType == 'outfit' then
-                    player:addOutfitAddon(itemId, 3)
-                    player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
-                    player:sendTextMessage(SHOP_MSG_TYPE, 'You received ' .. shopOfferName .. ' from Website Shop.')
-                    db.asyncQuery("DELETE FROM `z_ots_comunication` WHERE `id` = " .. transactionId)
-                    db.asyncQuery("UPDATE `z_shop_history_item` SET `trans_state`= 'realized', `trans_real`=" .. os.time() .. " WHERE `id` = " .. transactionId)
+                    local abc = OUTFIT_LIST[outfitParam]
+                    if abc then
+                        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Você recebeu o outfit ".. outfitParam ..".")
+                        player:addOutfitAddon(abc[1], 3)
+                        player:addOutfitAddon(abc[2], 3)
+                        player:getPosition():sendMagicEffect(31)
+                        db.asyncQuery("DELETE FROM `z_ots_comunication` WHERE `id` = " .. transactionId)
+                        db.asyncQuery("UPDATE `z_shop_history_item` SET `trans_state`= 'realized', `trans_real`=" .. os.time() .. " WHERE `id` = " .. transactionId)  
+                    end
                 end
             end
         until not result.next(resultId)
--- data/talkactions/scripts/addonDoll.lua
+++ data/talkactions/scripts/addonDoll.lua
@@ -1,35 +1,7 @@
-local outfits =
-{
-	--[outfit] = {id_female, id_male}
-	["citizen"] = {136, 128},
-	["hunter"] = {137, 129},
-	["mage"] = {138, 130},
-	["knight"] = {139, 131},
-	["noblewoman"] = {140, 132},
-	["summoner"] = {141, 133},
-	["warrior"] = {142, 134},
-	["barbarian"] = {147, 143},
-	["druid"] = {148, 144},
-	["wizard"] = {149, 145},
-	["oriental"] = {150, 146},
-	["pirate"] = {155, 151},
-	["assassin"] = {156, 152},
-	["beggar"] = {157, 153},
-	["shaman"] = {158, 154},
-	["norsewoman"] = {252, 251},
-	["nightmare"] = {269, 268},
-	["jester"] = {270, 273},
-	["brotherhood"] = {279, 278},
-	["demonhunter"] = {288, 289},
-	["yalaharian"] = {324, 325},
-	["warmaster"] = {336, 335},
-	["wayfarer"] = {366, 367}
-}
-
 function onSay(player, words, param)
 
 	local addondoll_id = 9693
-	local word = outfits[string.lower(param)]
+	local word = OUTFIT_LIST[string.lower(param)]
 
 	if param == "" or not word then
 		player:sendCancelMessage("Digite novamente, algo está errado!")
