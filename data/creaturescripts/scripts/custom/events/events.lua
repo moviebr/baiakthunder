f79e3cf78f8ab534b32c7a1617b82a9418bf6001
Movie <contatobrenoalves@gmail.com>
2020-08-28 11:57:02 -0300

    Update events.lua

--- data/creaturescripts/scripts/custom/events/events.lua
+++ data/creaturescripts/scripts/custom/events/events.lua
@@ -1,9 +1,9 @@
 function onPrepareDeath(player, killer)
-
+--[[
 	if Game.getStorageValue(BATTLEFIELD.storageEventStatus) >= 1 then
 		BATTLEFIELD:removePlayer(player:getId())
 	end
-
+--]]
 	player:setStorageValue(STORAGEVALUE_EVENTS, 0)
 	return true
 end
