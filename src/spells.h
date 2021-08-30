8abfa0dc3d3a329b7dafc87457cad3a15080820e
Breno Alves <contatobrenoalves@gmail.com>
2021-08-30 18:38:56 -0300

    Remove unused type alias for rune functions

--- src/spells.h
+++ src/spells.h
@@ -79,8 +79,6 @@ class Spells final : public BaseEvents
 		LuaScriptInterface scriptInterface { "Spell Interface" };
 };
 
-using RuneSpellFunction = std::function<bool(const RuneSpell* spell, Player* player, const Position& posTo)>;
-
 class BaseSpell
 {
 	public:
